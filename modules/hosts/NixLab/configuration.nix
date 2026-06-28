92c0d30e286f13e637cd19db1f8feb44e6f38758
Expressive-Synapse <ConnorDGoff@protonmail.com>
2026-06-28 18:29:41 -0400

    removed gnome

--- modules/hosts/NixLab/configuration.nix
+++ modules/hosts/NixLab/configuration.nix
@@ -59,10 +59,6 @@
       # Enable the X11 windowing system.
       services.xserver.enable = true;
 
-      # Enable the GNOME Desktop Environment.
-      services.displayManager.gdm.enable = true;
-      services.desktopManager.gnome.enable = true;
-
       # Configure keymap in X11
       # services.xserver.xkb.layout = "us";
       # services.xserver.xkb.options = "eurosign:e,caps:escape";
