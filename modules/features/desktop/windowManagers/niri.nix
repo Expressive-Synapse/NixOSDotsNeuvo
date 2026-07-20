cbeb3ccbada6648afc1d3de59abcb7b57000d254
Expressive-Synapse <ConnorDGoff@protonmail.com>
2026-07-20 13:06:20 -0400

    added noctalia

--- modules/features/desktop/windowManagers/niri.nix
+++ modules/features/desktop/windowManagers/niri.nix
@@ -6,71 +6,81 @@
       package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
     };
   };
-  perSystem = { pkgs, lib, ... }: {
+  perSystem =
+    {
+      pkgs,
+      lib,
+      self',
+      ...
+    }:
+    {
 
-    packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
-      inherit pkgs;
-      settings = {
-        input.keyboard = {
-          xkb.layout = "us,ua";
-        };
+      packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
+        inherit pkgs;
+        settings = {
+          spawn-at-startup = [
+            (lib.getExe self'.packages.myNoctalia)
+          ];
+          input.keyboard = {
+            xkb.layout = "us,ua";
+          };
 
-        prefer-no-csd = _: { };
+          prefer-no-csd = _: { };
 
-        layout.gaps = 5;
+          layout.gaps = 5;
 
-        hotkey-overlay.skip-at-startup = _: { };
-        binds = {
-          "Mod+Return".spawn-sh = lib.getExe pkgs.ghostty;
+          hotkey-overlay.skip-at-startup = _: { };
+          binds = {
+            "Mod+Return".spawn-sh = lib.getExe pkgs.ghostty;
 
-          "Mod+Q".close-window = _: { };
-          "Mod+F".maximize-column = _: { };
-          "Mod+Shift+F".fullscreen-window = _: { };
-          "Mod+V".toggle-window-floating = _: { };
+            "Mod+Q".close-window = _: { };
+            "Mod+F".maximize-column = _: { };
+            "Mod+Shift+F".fullscreen-window = _: { };
+            "Mod+V".toggle-window-floating = _: { };
 
-          "Mod+H".focus-column-left = _: { };
-          "Mod+L".focus-column-right = _: { };
-          "Mod+K".focus-window-up = _: { };
-          "Mod+J".focus-window-down = _: { };
+            "Mod+H".focus-column-left = _: { };
+            "Mod+L".focus-column-right = _: { };
+            "Mod+K".focus-window-up = _: { };
+            "Mod+J".focus-window-down = _: { };
 
-          "Mod+Left".focus-column-left = _: { };
-          "Mod+Right".focus-column-right = _: { };
-          "Mod+Up".focus-window-up = _: { };
-          "Mod+Down".focus-window-down = _: { };
+            "Mod+Left".focus-column-left = _: { };
+            "Mod+Right".focus-column-right = _: { };
+            "Mod+Up".focus-window-up = _: { };
+            "Mod+Down".focus-window-down = _: { };
 
-          "Mod+Shift+H".move-column-left = _: { };
-          "Mod+Shift+L".move-column-right = _: { };
-          "Mod+Shift+K".move-window-up = _: { };
-          "Mod+Shift+J".move-window-down = _: { };
+            "Mod+Shift+H".move-column-left = _: { };
+            "Mod+Shift+L".move-column-right = _: { };
+            "Mod+Shift+K".move-window-up = _: { };
+            "Mod+Shift+J".move-window-down = _: { };
 
-          "Mod+Shift+Left".move-column-left = _: { };
-          "Mod+Shift+Right".move-column-right = _: { };
-          "Mod+Shift+Up".move-window-up = _: { };
-          "Mod+Shift+Down".move-window-down = _: { };
+            "Mod+Shift+Left".move-column-left = _: { };
+            "Mod+Shift+Right".move-column-right = _: { };
+            "Mod+Shift+Up".move-window-up = _: { };
+            "Mod+Shift+Down".move-window-down = _: { };
 
-          "Mod+1".focus-workspace = "w0";
-          "Mod+2".focus-workspace = "w1";
-          "Mod+3".focus-workspace = "w2";
-          "Mod+4".focus-workspace = "w3";
-          "Mod+5".focus-workspace = "w4";
-          "Mod+6".focus-workspace = "w5";
-          "Mod+7".focus-workspace = "w6";
-          "Mod+8".focus-workspace = "w7";
-          "Mod+9".focus-workspace = "w8";
-          "Mod+0".focus-workspace = "w9";
+            "Mod+1".focus-workspace = "w0";
+            "Mod+2".focus-workspace = "w1";
+            "Mod+3".focus-workspace = "w2";
+            "Mod+4".focus-workspace = "w3";
+            "Mod+5".focus-workspace = "w4";
+            "Mod+6".focus-workspace = "w5";
+            "Mod+7".focus-workspace = "w6";
+            "Mod+8".focus-workspace = "w7";
+            "Mod+9".focus-workspace = "w8";
+            "Mod+0".focus-workspace = "w9";
 
-          "Mod+Shift+1".move-column-to-workspace = "w0";
-          "Mod+Shift+2".move-column-to-workspace = "w1";
-          "Mod+Shift+3".move-column-to-workspace = "w2";
-          "Mod+Shift+4".move-column-to-workspace = "w3";
-          "Mod+Shift+5".move-column-to-workspace = "w4";
-          "Mod+Shift+6".move-column-to-workspace = "w5";
-          "Mod+Shift+7".move-column-to-workspace = "w6";
-          "Mod+Shift+8".move-column-to-workspace = "w7";
-          "Mod+Shift+9".move-column-to-workspace = "w8";
-          "Mod+Shift+0".move-column-to-workspace = "w9";
+            "Mod+Shift+1".move-column-to-workspace = "w0";
+            "Mod+Shift+2".move-column-to-workspace = "w1";
+            "Mod+Shift+3".move-column-to-workspace = "w2";
+            "Mod+Shift+4".move-column-to-workspace = "w3";
+            "Mod+Shift+5".move-column-to-workspace = "w4";
+            "Mod+Shift+6".move-column-to-workspace = "w5";
+            "Mod+Shift+7".move-column-to-workspace = "w6";
+            "Mod+Shift+8".move-column-to-workspace = "w7";
+            "Mod+Shift+9".move-column-to-workspace = "w8";
+            "Mod+Shift+0".move-column-to-workspace = "w9";
+          };
         };
       };
     };
-  };
 }
